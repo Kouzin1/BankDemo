05bbc480e762c68b62135d5979bd2dca8f2161c5
kouzin1 <samuelakinwunmi2@gmail.com>
2024-01-18 19:32:47 +0000

    Update

--- Desktop/Personal Project/agba_js/Bank_Demo/script.js
+++ Desktop/Personal Project/agba_js/Bank_Demo/script.js
@@ -63,7 +63,7 @@ document.querySelectorAll(".nav__link").forEach(function (el) {
     e.preventDefault();
     const id = this.getAttribute("href");
     console.log(id);
-    document.querySelector(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
 // console.log(document.documentElement);
