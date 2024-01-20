a05e5430b3e60a0fd60cbeba4301b6fea0fe0a4a
kouzin1 <samuelakinwunmi2@gmail.com>
2024-01-20 22:51:13 +0000

    Sticky Navigation

--- Desktop/Personal Project/agba_js/Bank_Demo/script.js
+++ Desktop/Personal Project/agba_js/Bank_Demo/script.js
@@ -121,6 +121,14 @@ const handleHover = function (e, opacity) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
