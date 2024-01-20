5ba02472deddb1cae09b4f51e1fb3e963cd21758
kouzin1 <samuelakinwunmi2@gmail.com>
2024-01-20 22:07:11 +0000

    Update

--- Desktop/Personal Project/agba_js/Bank_Demo/script.js
+++ Desktop/Personal Project/agba_js/Bank_Demo/script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations_tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Modal window
@@ -78,9 +82,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 //Tabbed component
-const tabs = document.querySelectorAll(".operations_tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
